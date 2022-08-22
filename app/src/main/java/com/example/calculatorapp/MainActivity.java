33f12be0fe6a857748769e8c976001bb3187c7cb
EnguerranP <90780255+EnguerranP@users.noreply.github.com>
2022-08-22 11:01:03 -0500

    8/22 Calculator App should be working
    It should work but i haven't tested it yet due to some technical difficulties with android studio.

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,12 +3,82 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Addition(View view){
+        EditText firstInput = findViewById(R.id.editTextFirstInput);
+        //Getting the edit Text into a string
+        String firstValueString =firstInput.getText().toString();
+
+        int  firstValue = Integer.parseInt(firstValueString); //This is the first input value
+
+        EditText secondInput = findViewById(R.id.editTextSecondInput);
+
+        String secondValueString = secondInput.getText().toString();
+
+        int secondValue = Integer.parseInt(secondValueString); //This is the second input value
+
+        int result = firstValue + secondValue;
+
+        TextView resultView = findViewById(R.id.resultDisplay);
+
+        resultView.setText(""+result); //result view should display the value
+
+
+    }
+    public void Subtraction(View view){
+        EditText firstInput = findViewById(R.id.editTextFirstInput);
+        String firstValueString =firstInput.getText().toString();
+        int  firstValue = Integer.parseInt(firstValueString);
+        EditText secondInput = findViewById(R.id.editTextSecondInput);
+        String secondValueString = secondInput.getText().toString();
+        int secondValue = Integer.parseInt(secondValueString); //This is the second input value
+
+        int result = firstValue - secondValue;
+
+        TextView resultView = findViewById(R.id.resultDisplay);
+
+        resultView.setText(""+result); //result view should display the value
+
+
+
+    }
+    public void Multiplication(View view){
+        EditText firstInput = findViewById(R.id.editTextFirstInput);
+        String firstValueString =firstInput.getText().toString();
+        int  firstValue = Integer.parseInt(firstValueString);
+        EditText secondInput = findViewById(R.id.editTextSecondInput);
+        String secondValueString = secondInput.getText().toString();
+        int secondValue = Integer.parseInt(secondValueString); //This is the second input value
+
+        int result = firstValue * secondValue;
+
+        TextView resultView = findViewById(R.id.resultDisplay);
+
+        resultView.setText(""+result); //result view should display the value
+    }
+    public void Division(View view){
+        EditText firstInput = findViewById(R.id.editTextFirstInput);
+        String firstValueString =firstInput.getText().toString();
+        int  firstValue = Integer.parseInt(firstValueString);
+        EditText secondInput = findViewById(R.id.editTextSecondInput);
+        String secondValueString = secondInput.getText().toString();
+        int secondValue = Integer.parseInt(secondValueString); //This is the second input value
+
+        int result = firstValue / secondValue;
+
+        TextView resultView = findViewById(R.id.resultDisplay);
+
+        resultView.setText(""+result); //result view should display the value
+
     }
 }
